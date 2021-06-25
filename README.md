12cf53c0fb6e2884d5754d697067436471c6cbb7
Peter Schwede <pschwede@posteo.de>
2021-06-25 23:01:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# zim2md
+
+Executable Python module for Zim Wiki to Markdown conversion.
+
+## Command Line Interface
+
+### Install
+LI
+Just have python3 for your default Python engine.
+
+### Usage
+
+```bash
+./zim2md.py <input.file >output.file
+```
+
+If you are on Windows, you may be required to your python engine in the command line. May be like so:
+```cmd
+C:\Programs\Python3.8\python3.exe zim2md.py <input.file >output.file
+```
+
+## Python module
+
+### Install
+
+Clone this Github project as a submodule:
+```bash
+git submodule add https://github.com/pschwede/zim2md.git zim2md
+```
+
+### Usage
+
+Example:
+
+```python
+from zim2md import zim2md
+
+with open("input.file", "r") as _f:
+	print(zim2md.translate(_f.readlines()))
+```
---
+++ zim2md.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+"""
+Converts [Zim Desktop Wiki](https://zim-wiki.org) to Markdown
+
+Usage:
+    python zim2md.py <input.file >output.file
+"""
+
+from typing import List
+from re import sub, fullmatch
+
+
+def __compatible(lines):
+    """Return True iff the first two lines of a file allute to it being
+    convertible or not."""
+    if len(lines) < 2:
+        return False
+    if not fullmatch("^Content-Type: text/x-zim-wiki$", lines[0].strip()):
+        return False
+    if not fullmatch("^Wiki-Format: zim 0.[0-6]$", lines[1].strip()) is not None:
+        return False
+    return True
+
+
+def compatible(path=None, infile=None, lines=None):
+    """Return True iff the given path points to a Zim Wiki file."""
+    if open is not None:
+        with open(path, "r") as _f:
+            return __compatible(_f.readlines()[4:])
+    elif infile is not None:
+        return __compatible(infile.readlines()[4:])
+    elif lines is not None:
+        return __compatible(lines[4:])
+    return True
+
+
+def translate(text: List[str], path:str = "") -> List[str]:
+    """Discards the first four lines. All other lines are converted."""
+    # The first 4 lines usually contain file format info.
+    text = text[4:]
+    for i, line in enumerate(text):
+        # Head lines
+        line = sub(r"^(=+)([^=]+)=+$", r"\g<1>\g<2>", line)
+        line = sub(r"^======", "#", line)
+        line = sub(r"^=====", "##", line)
+        line = sub(r"^====", "###", line)
+        line = sub(r"^===", "####", line)
+        line = sub(r"^==", "#####", line)
+        line = sub(r"^=", "######", line)
+
+        # Links
+        line = sub(r"\[\[(.+?)\|(.+?)\]\]", r"[\g<2>](\g<1>)", line)
+        line = sub(r"(?<=\[\[)\+([^+:]*)", sub(r"\..{2,4}$", "/", path) + r"\g<1>", line)
+        line = sub(r"(?<=\[\[)([^: ]*) ", r"\g<1>_", line)
+        line = sub(r"(?<=\[\[)([^\]]*):", r"\g<1>/", line)
+        line = sub(r"\[\[([^\]]+)\]\]", r"[\g<1>](\g<1>.txt)", line)
+
+        # Lists
+        line = sub(r"^(\s*)\[[*]\]", r"\g<1>- [x]", line, count=1)
+        line = sub(r"^(\s*)\[[x]\]", r"\g<1>- [-]", line, count=1)
+        line = sub(r"^(\s*)\[[ ]\]", r"\g<1>- [ ]", line, count=1)
+        # TODO indented list elements without dots or checkboxes
+
+        text[i] = line
+
+    # TODO more features
+    return text
+
+
+if __name__ == "__main__":
+    import sys
+    ls = sys.stdin.readlines()
+    if compatible(lines=ls):
+        sys.stdout.writelines(translate(text=ls))
+    else:
+        sys.stderr.writelines(["FATAL: Incompatible file."])
+        sys.exit(1)
